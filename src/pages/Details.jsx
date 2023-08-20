caae7212f24080b83ad03a082f460beccf0e5951
Aandi1989 <navaliny1989@gmail.com>
2023-08-20 18:34:21 +0300

    detail page in process

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useHistory, useParams} from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
 
-export const Details = ({match}) => {
+export const Details = () => {
+  const {name} = useParams();
+  const {push, goBack} = useHistory();
+  const [country, setCountry] = useState(null);
+
+  console.log(country)
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(
+      ({data}) => setCountry(data[0])
+    )
+  }, [name]);
+
   return (
-    <div>Details {match.params.name}</div>
+    <div>
+      <Button onClick={goBack}>
+        <IoArrowBack/> Back
+      </Button>
+      {country && <Info push={push} {...country}/>}
+    </div>
   )
 }
